297599ef3ba74cf0862decf0baa30c48a824af13
Christianfirecup <Christianfirecup@aol.com>
2025-04-04 21:18:01 -0400

    switching over to assistant api

--- src/apis/APIHandlers.py
+++ src/apis/APIHandlers.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import requests
+import json
 
 load_dotenv()
 OAIkey = os.getenv("OAI")
@@ -8,12 +10,20 @@ OAIkey = os.getenv("OAI")
 
 client = OpenAI(api_key=OAIkey)
 
-def OpenAICall(model, uinput):
-    response = client.responses.create(
-        model=model,
-        input= uinput
+def Create_Assistant(name, instructions, tools, model):
+    return client.beta.assistants.create(
+        name=name,
+        instructions=instructions,
+        tools=tools,
+        model=model
     )
     return response.output_text
+
+def Create_Thread():
+    return client.beta.threads.create()
+
+def OpenRouterCall(model, uinput, chatid):
+      print("todo")
 
 
 #print(OpenAICall("gpt-4o", "What is the study of calculus"))
