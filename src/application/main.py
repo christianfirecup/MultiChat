a1d290617ed296fbf2a4c743cd1d0958e6e201fa
jeffreyfranc28 <jeffreyfrancois2024@gmail.com>
2025-04-05 02:34:54 -0400

    Commented Task

--- src/application/main.py
+++ src/application/main.py
@@ -1,5 +1,7 @@
-print("Welcome to MultiChat")
+print("Welcome to MultiChat!")
+print("Which Large Language Model would you like to interact with?")
+userinput = input()
 
-
+print(f"Accessing {userinput} now")
 #Create Menu [What LLM user would like to speak]
 
