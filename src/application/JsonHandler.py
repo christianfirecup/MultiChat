8cdd835b1d9ba56085b5c452b384e196d936d3a7
jeffreyfranc28 <jeffreyfrancois2024@gmail.com>
2025-04-07 19:46:55 -0400

    fixed git

---
+++ src/application/JsonHandler.py
@@ -0,0 +1,27 @@
+import json
+
+data = {
+    "1": "o3-mini",
+
+    "2": "o1-mini",
+
+    "3": "o1",
+
+    "4": "dall.e 3",
+
+    "5": "gpt-4o",
+
+    "6": "gpt-4.5 preview",
+
+    "7": "chatgpt-4o",
+    
+    "8": "gpt-4o mini",
+}
+
+with open("data.json", "w") as json_file:
+    json.dump(data, json_file, indent=4)
+
+with open("data.json", "r") as file:
+    loaded_data = json.load(file)
+    print(loaded_data)
+    print("completed")
