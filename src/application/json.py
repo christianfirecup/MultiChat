5389afa1e6d36bdfd060b30760ea79cf732bf198
jeffreyfranc28 <jeffreyfrancois2024@gmail.com>
2025-04-06 23:37:11 -0400

    Commented Task

---
+++ src/application/json.py
@@ -0,0 +1,25 @@
+import json
+
+data = {
+    "1": "o3-mini",
+
+    "2": "o1-mini",
+
+    "3": "o1",
+
+    "4": "o1-pro",
+
+    "5": "gpt-4o",
+
+    "6": "gpt-4.5 preview",
+
+    "7": "chatgpt-4o",
+    
+    "8": "gpt-4o mini",
+
+
+
+}
+
+
+
--- src/application/main.py
+++ src/application/main.py
@@ -1,6 +1,8 @@
 print("Welcome to MultiChat!")
 print("Which Large Language Model would you like to interact with?")
 userinput = input()
+text = userinput
+lower_text = text.lower()
 
 print(f"Accessing {userinput} now")
 #Create Menu [What LLM user would like to speak]
