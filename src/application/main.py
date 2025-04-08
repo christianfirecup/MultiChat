8a800a25d9560d45a495d68e6e4e50b99c2541d3
jeffreyfranc28 <jeffreyfrancois2024@gmail.com>
2025-04-07 21:08:33 -0400

    Display LLMs and link to json

--- src/application/main.py
+++ src/application/main.py
@@ -1,10 +1,30 @@
+import json
+import os
+
+#create 2: one for openai, one for every other model
+file_path = 'model.json'
+if not os.path.exists(file_path):
+    with open(file_path, 'w') as f:
+        json.dump([], f)
+
 print("Welcome to MultiChat!")
+print("Here are the Large Language Models we have avaliable: 1 - o3-mini, 2 - o1-mini, 3 - o1, 4 - dall.e, 5 - gpt-4o, 6 - gpt-4.5 preview, 7 - chatgpt-4o, 8 - gpt-4o mini")
 print("Which Large Language Model would you like to interact with?")
 userinput = input()
 text = userinput
 lower_text = text.lower()
 
 print(f"Accessing {userinput} now")
-#todo
-#Create Menu [What LLM user would like to speak]
+
+with open(file_path, 'r') as f:
+    data = json.load(f)
+
+data.append({"model": userinput})
+with open(file_path, 'w') as f:
+    json.dump(data, f, indent=4)
+
+
+
+
+
 
