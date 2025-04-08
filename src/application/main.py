4895ebcd5ea59d54c21a0b84e485ae57ef746255
jeffreyfranc28 <jeffreyfrancois2024@gmail.com>
2025-04-08 02:31:33 -0400

    removed numbers in print

--- src/application/main.py
+++ src/application/main.py
@@ -7,7 +7,7 @@ print("Welcome to MultiChat!")
 print("What Provider would you like to use: openai, openrouter")
 provider = input()
 if provider.lower() == "openai":
-    print("Here are the Large Language Models we have avaliable: 1 - o3-mini, 2 - o1-mini, 3 - o1, 4 - gpt-4o, 5 - gpt-4.5 preview, 6 - chatgpt-4o, 7 - gpt-4o mini")
+    print("Here are the Large Language Models we have avaliable: o3-mini, o1-mini, o1, gpt-4o, gpt-4.5 preview, chatgpt-4o, gpt-4o mini")
     print("Which Large Language Model would you like to interact with?")
     userinput = input()
     text = userinput
