0a70d9e27bbb3edf61e4fab260b87e2ddda9d5ae
jeffreyfranc28 <jeffreyfrancois2024@gmail.com>
2025-04-08 01:23:58 -0400

    add the openai key and calls it

--- src/application/JsonHandler.py
+++ src/application/JsonHandler.py
@@ -7,21 +7,19 @@ data = {
 
     "3": "o1",
 
-    "4": "dall.e 3",
+    "4": "gpt-4o",
 
-    "5": "gpt-4o",
+    "5": "gpt-4.5 preview",
 
-    "6": "gpt-4.5 preview",
-
-    "7": "chatgpt-4o",
+    "6": "chatgpt-4o",
     
-    "8": "gpt-4o mini",
+    "7": "gpt-4o mini",
 }
 
-with open("model.json", "w") as json_file:
+with open("openai.json", "w") as json_file:
     json.dump(data, json_file, indent=4)
 
-with open("model.json", "r") as file:
+with open("openai.json", "r") as file:
     loaded_data = json.load(file)
     print(loaded_data)
     print("completed")
--- src/application/main.py
+++ src/application/main.py
@@ -1,25 +1,26 @@
 import json
 import os
+from src.apis.APIHandlers import OpenAI
 
 #create 2: one for openai, one for every other model
-file_path = 'model.json'
-if not os.path.exists(file_path):
+file_path = 'openai.json'
+if not os.path.exists(file_path) or os.stat(file_path).st_size == 0:
     with open(file_path, 'w') as f:
         json.dump([], f)
 
 print("Welcome to MultiChat!")
-print("Here are the Large Language Models we have avaliable: 1 - o3-mini, 2 - o1-mini, 3 - o1, 4 - dall.e, 5 - gpt-4o, 6 - gpt-4.5 preview, 7 - chatgpt-4o, 8 - gpt-4o mini")
+print("Here are the Large Language Models we have avaliable: 1 - o3-mini, 2 - o1-mini, 3 - o1, 4 - gpt-4o, 5 - gpt-4.5 preview, 6 - chatgpt-4o, 7 - gpt-4o mini")
 print("Which Large Language Model would you like to interact with?")
 userinput = input()
 text = userinput
 lower_text = text.lower()
 
 print(f"Accessing {userinput} now")
+response =
 
 with open(file_path, 'r') as f:
     data = json.load(f)
 
-data.append({"model": userinput})
 with open(file_path, 'w') as f:
     json.dump(data, f, indent=4)
 
